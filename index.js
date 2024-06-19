a4421dbb1d24eed8f98bbefc81ca3f4730ad9c26
Aspireve <steve1818fernandes@gmail.com>
2024-06-19 15:35:59 +0530

    Update

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require("http")
+const userdata = require("./userdata")
 
-const server = http.createServer((req, res) => {
-    res.end("hello World")
-
+const server = http.createServer(function(req, res) => {
+    res.setHeader(200, {"Content-type": "text/html"})
+    res.write("First name is: ", userdata.firstname)
+    res.write("")
+    // res.end("hello World")
+    res.send()
 })
 
 
